6337864c54b287c0ddf66de3d85405eb55c9c2c6
Omer Kenet <omerk@wix.com>
2024-09-22 18:37:29 +0300

    Initial commit

---
+++ env.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="@remix-run/node" />
+/// <reference types="vite/client" />
+
+declare module '*.svg' {
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<
+        React.ComponentProps<'svg'> & { title?: string }
+    >;
+    export default ReactComponent;
+}
+
+interface ClientENV {
+    readonly WIX_CLIENT_ID?: string;
+    // more env variables...
+}
+
+declare interface Window {
+    ENV: ClientENV;
+}
